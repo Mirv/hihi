d0597b1e4302d5915d613b9fe6ce18d8727ea5f5
Matt <reski.raptor@gmail.com>
2017-03-01 02:48:07 +0000

    test

--- lib/hihi.rb
+++ lib/hihi.rb
@@ -1,7 +1,9 @@
 require "hihi/version"
 
-module Hihi
+class Hihi < Rails::Railtie
   def self.hi
     puts "Hi! Hi! world!"
   end
 end
+
+# https://www.digitalocean.com/community/tutorials/how-to-package-and-distribute-ruby-applications-as-a-gem-using-rubygems
